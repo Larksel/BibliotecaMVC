b60f8b1f2c60b918d7dddf25e90e4726deda41e8
Larksel <lemuelgames@outlook.com>
2024-05-22 16:17:09 -0300

    View da tabela de emprestimos criada

---
+++ Controllers/EmprestimoController.cs
@@ -0,0 +1,22 @@
+﻿using Biblioteca.Data;
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimoModel> emprestimos = _db.Emprestimo;
+            return View(emprestimos);
+        }
+    }
+}
